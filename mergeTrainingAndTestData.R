f00ad0f24e04175187530bb75e3c6f2dd8b70155
fabianobr <fabiano@freitassilva.com>
2015-08-23 16:29:35 -0300

    stats data

--- mergeTrainingAndTestData.R
+++ mergeTrainingAndTestData.R
@@ -1,3 +1,10 @@
+source("collectTestData.R")
+
 mergeTrainingAndTestData <- function() {
+    
+    statsData <- collectTestData()
+    
+    write.table(a,"courseProjectStats.txt", row.name=FALSE)
+    
     print("mergeTrainingAndTestData OK")
 }
